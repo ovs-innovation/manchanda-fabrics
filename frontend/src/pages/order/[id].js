ab5d1c1b2b776f0e8cd63d3a92091bda594887e9
Saurabh Singh <saurabhxxx6@gmail.com>
2026-09-07 15:09:18 +0530

    fix(frontend): restore missing trackingNumber condition tags in order details

--- frontend/src/pages/order/[id].js
+++ frontend/src/pages/order/[id].js
@@ -149,8 +149,8 @@ const Order = ({ params }) => {
                 )}
                 content={() => printRef.current}
               />
-
-
+               {data.trackingNumber && (
+                 <>
                    <button 
                      onClick={() => handleCopyTracking(data.trackingNumber)}
                      className="flex items-center justify-center bg-gray-100 text-gray-700 transition-all font-serif text-sm font-semibold h-10 py-2 px-5 rounded-md hover:bg-gray-200 shadow-sm"
